afa5429df2733a913e6eb5892277d2b63793d5f4
Krishna Tadepalli <krishna.tadepalli@gmail.com>
2020-11-22 22:10:54 +0530

    Added request validation for Post request

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator')
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  public
-router.get('/', (req, res) => {
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with six or more characters').isLength({min: 6})
+], (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
     res.send('User route');
 })
 
